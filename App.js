10978ab2aa97321173a84c3f5eae2d90c432c423
basilspice <basil.breton@gmail.com>
2023-12-11 16:37:59 -0800

    created homescreen

--- App.js
+++ App.js
@@ -2,16 +2,14 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View, SafeAreaView } from "react-native";
 import { Provider } from "react-redux";
 import { store } from "./store";
+import HomeScreen from "./screens/HomeScreen";
 
 export default function App() {
   return (
     <Provider store={store}>
-      
-        <View style={styles.container}>
-          <Text>uber clone incoming</Text>
-          <StatusBar style="auto" />
-        </View>
-      
+      <View style={styles.container}>
+        <HomeScreen />
+      </View>
     </Provider>
   );
 }
