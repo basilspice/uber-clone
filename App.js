3fb6509c735a6d57b52c55f541912fb54fc119c5
basilspice <basil.breton@gmail.com>
2023-12-12 12:04:53 -0800

    added google maps, updated babel

--- App.js
+++ App.js
@@ -3,17 +3,28 @@ import { StyleSheet, Text, View, SafeAreaView } from "react-native";
 import { Provider } from "react-redux";
 import { store } from "./store";
 import HomeScreen from "./screens/HomeScreen";
-import tw from 'twrnc';
+import tw from "twrnc";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import MapScreen from "./screens/MapScreen";
+import EatScreens from "./screens/EatScreens";
+
+const Stack = createNativeStackNavigator();
+
 
 export default function App() {
   return (
     <Provider store={store}>
-      <View >
-        <HomeScreen />
-      </View>
+      <NavigationContainer>
+        <SafeAreaProvider>
+        <Stack.Navigator>
+        <Stack.Screen name="HomeScreen" component={HomeScreen} options={{headerShown: false,}} />
+        <Stack.Screen name="MapScreen" component={MapScreen} options={{headerShown: false,}}/>
+        <Stack.Screen name="EatsScreen" component={EatScreens} options={{headerShown: false,}}/>
+      </Stack.Navigator>
+        </SafeAreaProvider>
+      </NavigationContainer>
     </Provider>
   );
 }
-
-
-
---
+++ screens/EatScreens.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const EatScreens = () => {
+  return (
+    <View>
+      <Text>EatScreens</Text>
+    </View>
+  )
+}
+
+export default EatScreens
