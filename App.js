7e242ea007951cad1768a12738cb45ee44ac172b
basilspice <basil.breton@gmail.com>
2023-12-12 10:51:43 -0800

    tailwind classnames package working

--- App.js
+++ App.js
@@ -3,22 +3,17 @@ import { StyleSheet, Text, View, SafeAreaView } from "react-native";
 import { Provider } from "react-redux";
 import { store } from "./store";
 import HomeScreen from "./screens/HomeScreen";
+import tw from 'twrnc';
 
 export default function App() {
   return (
     <Provider store={store}>
-      <View style={styles.container}>
+      <View >
         <HomeScreen />
       </View>
     </Provider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+
+
